4c7557fae365b5338eb668ac87e3b3be779a73bb
Sunki Baek <sunki.baek@gmail.com>
2020-08-25 08:09:37 -0700

    feat: add param parsings with post endpoint

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -31,6 +31,14 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    %{
+      conv
+      | status: 201,
+        resp_body: "Createa a #{conv.params["type"]} bear named #{conv.params["name"]} bear!"
+    }
+  end
+
   def route(%Conv{method: "GET", path: "/about"} = conv) do
     @pages_path
     |> Path.join("about.html")
