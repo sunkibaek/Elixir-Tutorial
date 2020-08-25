50f3c51ba2c296d642a2f53a62748b51d1dea120
Sunki Baek <sunki.baek@gmail.com>
2020-08-24 19:15:39 -0700

    refactor: refactor using function clause

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -40,24 +40,26 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    file =
-      Path.expand("../../pages", __DIR__)
-      |> Path.join("about.html")
-
-    case File.read(file) do
-      {:ok, content} ->
-        %{conv | status: 200, resp_body: content}
-
-      {:error, :enoent} ->
-        %{conv | status: 404, resp_body: "File not found!"}
-
-      {:error, reason} ->
-        %{conv | status: 500, resp_body: "File error: #{reason}"}
-    end
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
   end
 
   def route(%{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
+  end
+
+  defp handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content}
+  end
+
+  defp handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not found!"}
+  end
+
+  defp handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
   def track(%{status: 404, path: path} = conv) do
