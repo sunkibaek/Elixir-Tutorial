f030aa2b5513328d0556641589197346ea3ffea8
Sunki Baek <sunki.baek@gmail.com>
2020-08-24 19:19:53 -0700

    feat: add doc with module attributes
    - also extract module constants

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,4 +1,9 @@
 defmodule Servy.Handler do
+  @moduledoc "Handles HTTP requests."
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  @doc "Transforms the request into a response."
   def handle(request) do
     request
     |> parse()
@@ -40,7 +45,7 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    Path.expand("../../pages", __DIR__)
+    @pages_path
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
@@ -67,6 +72,7 @@ defmodule Servy.Handler do
     conv
   end
 
+  @doc "Logs 404 requests."
   def track(conv), do: conv
 
   def format_response(conv) do
