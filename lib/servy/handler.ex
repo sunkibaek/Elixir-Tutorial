8bceea3c79a3df464d8f741cb6ccddc8d31506f4
Sunki Baek <sunki.baek@gmail.com>
2020-08-26 14:18:10 -0700

    test: add tests

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -71,15 +71,3 @@ defmodule Servy.Handler do
     """
   end
 end
-
-request = """
-GET /wildthings HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-
-response = Servy.Handler.handle(request)
-
-IO.puts(response)
