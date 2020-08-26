3a9be2192ef490115157dbe40a67f674b93e2c01
Sunki Baek <sunki.baek@gmail.com>
2020-08-25 18:51:35 -0700

    refactor: refactor using enums

---
+++ lib/servy/wildthings.ex
@@ -0,0 +1,19 @@
+defmodule Servy.Wildthings do
+  alias Servy.Bear
+
+  def list_bears do
+    [
+      %Bear{id: 1, name: "Teddy", type: "Brown", hibernating: true},
+      %Bear{id: 2, name: "Smokey", type: "Black"},
+      %Bear{id: 3, name: "Paddington", type: "Brown"}
+    ]
+  end
+
+  def get_bear(id) when is_integer(id) do
+    Enum.find(list_bears(), fn b -> b.id == id end)
+  end
+
+  def get_bear(id) when is_binary(id) do
+    id |> String.to_integer() |> get_bear()
+  end
+end
